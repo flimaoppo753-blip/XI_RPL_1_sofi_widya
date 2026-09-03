0737ee8dac6278fe884dfbfc1c4d2d949f82e64e
unknown <flimaoppo753@gmail.com>
2026-09-03 14:29:06 +0700

    praktikum XI RPL 1

---
+++ script.js
@@ -0,0 +1,14 @@
+function tampilkanNama(){
+    document.getElementById("namaAnggota").innerHTML = `
+    <ol>
+        <style="list-style-type: decimal;
+        padding-left:5%;">
+        <li>sofi (flimaoppo753@gmail.com)</li>
+        <li>widya (widyautami54@gmail.com)</li>
+    </ol>
+
+        <button onclick="location.reload()">
+            tutup kembali
+        </button>
+    `;
+}                                                                                                                                                                                                                                                                                                                             
